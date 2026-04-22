a408011854dd456fdffa8abb8700a8035faf12a1
Lauren Brown <brownl6@mail.gvsu.edu>
2026-04-22 11:13:24 -0400

    Deployment issues for Pages.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "vuetify/styles";
 import App from "./App.vue";
 import router from "./router";
-
 const app = createApp(App);
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 app.use(createPinia());
+app.use(vuetify); // You need this for the styles to work!
 app.use(router);
 
 app.mount("#app");
