4773faabc4002ae76c00d8fca3d037d168bcae06
Lauren Brown <brownl6@mail.gvsu.edu>
2026-04-22 11:03:34 -0400

    Had to fix display issues.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,6 @@
+/// <reference types="vite/client" />
+/// <reference types="vuetify" />
+
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
